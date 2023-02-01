7b4f25b3d7d531945173657a11c4bf08d394c1ed
Sewa <sewabobrov04@gmail.com>
2023-02-01 13:23:58 +0100

    Initial commit

--- esse/settings.py
+++ esse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gjxkqthi20pmpjrb9@j9zi)&q$0_hur9@t69-mr$3a*z)jgp5*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
